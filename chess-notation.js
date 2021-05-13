3ffc73368bfab127b25a410c459a2a7e5d91ba4a
Luyu Zhou <luyu5710@gmail.com>
2021-05-13 21:01:05 +0200

    no loops

--- chess-notation.js
+++ chess-notation.js
@@ -4,34 +4,34 @@ function chessNotation(notation) {
 	const matches = notation.match(/[1-8]/g);
 
 	const emptySquare = '*';
-	for (let i = 0; i < matches.length; i++) {
-		notation = notation.replace(matches[i], emptySquare.padStart(matches[i], '*'));
-	}
+	matches.map((x, i) => {
+		notation = notation.replace(x, emptySquare.padStart(x, '*'));
+	})
 
 	let board = notation.split('/');
 	let newBoard = [];
 
-	for (let j = 0; j < 8; j++) {
-		board[j] = board[j].split('');
-		newBoard[j] = [];
-	}
+	board.map((x, i) => {
+		board[i] = board[i].split('');
+		newBoard[i] = [];
+	})
 
-	for (let i = 0; i < 8; i++) {
-		for (let j = 0; j < 8; j++) {
-		newBoard[i][j] = board[7-j][i];
-		};
-	}
+	board.map((x, i) => {
+		board[i].map((y, j) => {
+			newBoard[i][j] = board[7-j][i];
+		})
+	})
 
-	for (let i = 0; i < 8; i++) {
+	newBoard.map((x, i) => {
 		newBoard[i] = newBoard[i].join('');
-	}
+	})
 
 	let newNotation = newBoard.join('/');
 	const squares = newNotation.match(/[\*]+/g);
 
-	for (let i = 0; i < squares.length; i++) {
-		newNotation = newNotation.replace(squares[i], squares[i].length);
-	}
+	squares.map((x, i) => {
+		newNotation = newNotation.replace(x, x.length);
+	})
 
 	return newNotation;
 }
